ee4e0b3c941b55b35e62b302b290c8e20c22237b
Arya P P <aryapp2019@gmail.com>
2025-07-21 17:39:42 +0530

    model import fix

--- Client/src/assets/assets.js
+++ Client/src/assets/assets.js
@@ -15,24 +15,23 @@ export const assets = {
 }
 
 export const dummyTrailers = [
-  {
-    image: "https://img.youtube.com/vi/rR_2ti4l3nM/maxresdefault.jpg",
-    videoUrl: "https://www.youtube.com/watch?v=rR_2ti4l3nM"
-  },
-  {
-    image: "https://img.youtube.com/vi/w6MFx62SWxM/maxresdefault.jpg",
-    videoUrl: "https://www.youtube.com/watch?v=w6MFx62SWxM"
-  },
-  {
-    image: "https://img.youtube.com/vi/OsMqr3556F8/maxresdefault.jpg",
-    videoUrl: "https://www.youtube.com/watch?v=OsMqr3556F8"
-  },
-  {
-    image: "https://img.youtube.com/vi/acCVmR5RrN0/maxresdefault.jpg",
-    videoUrl: "https://www.youtube.com/watch?v=acCVmR5RrN0"
-  }
-];
-
+    {
+        image: "https://img.youtube.com/vi/WpW36ldAqnM/maxresdefault.jpg",
+        videoUrl: 'https://www.youtube.com/watch?v=WpW36ldAqnM'
+    },
+    {
+        image: "https://img.youtube.com/vi/-sAOWhvheK8/maxresdefault.jpg",
+        videoUrl: 'https://www.youtube.com/watch?v=-sAOWhvheK8'
+    },
+    {
+        image: "https://img.youtube.com/vi/1pHDWnXmK7Y/maxresdefault.jpg",
+        videoUrl: 'https://www.youtube.com/watch?v=1pHDWnXmK7Y'
+    },
+    {
+        image: "https://img.youtube.com/vi/umiKiW4En9g/maxresdefault.jpg",
+        videoUrl: 'https://www.youtube.com/watch?v=umiKiW4En9g'
+    },
+]
 
 const dummyCastsData = [
     { "name": "Milla Jovovich", "profile_path": "https://image.tmdb.org/t/p/original/usWnHCzbADijULREZYSJ0qfM00y.jpg", },
